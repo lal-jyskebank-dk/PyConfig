38882b64a88d999fba65ea899b934caf80c2a5b7
JB6504 <lal@jyskebank.dk>
2024-05-01 12:10:38 +0200

    tilføjet config

--- src/app.py
+++ src/app.py
@@ -1,3 +1,12 @@
+import configparser
 import streamlit as st
 
 st.header("Welcome to Streamlit! 👋")
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+st.write(config["DATABASE"]["name"])
+st.write(config["API"]["key"])
+st.write(config["API"]["url"])
+
